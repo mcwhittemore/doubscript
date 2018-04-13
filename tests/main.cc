595a440ec9647d5a0ad5b64d790ff6d99da92eb1
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2018-04-13 01:26:37 -0400

    show bad diffs and weird var cases

--- tests/main.cc
+++ tests/main.cc
@@ -5,5 +5,6 @@ int main(int argc, char **argv)
   int failures = 0;
   failures += test::check("./tests/maths.doub", "./tests/fixtures/maths.log", {8});
   failures += test::check("./tests/funcs.doub", "./tests/fixtures/funcs.log", {5,8});
+  failures += test::check("./tests/func-in-return.doub", "./tests/fixtures/func-in-return.log", {8});
   return failures;
 }
