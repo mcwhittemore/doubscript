65d8e3b4d47dc44cf416d72ca1801803b84a1a72
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2018-04-14 22:46:16 -0400

    add comparisons to the langauge

--- tests/main.cc
+++ tests/main.cc
@@ -20,9 +20,25 @@ int main(int argc, char **argv)
     doub::Doubscript logics = doub::Doubscript::fromFile("./tests/scripts/logics.doub");
     failures += assert::equal(logics.run("add", {2,3}), 5, "2 + 3 = 5");
     failures += assert::equal(logics.run("double", {2}), 4, "add(2,2) = 2 + 2 = 4");
-    // TODO test `return(func(a,a))`
     // TODO check that nothing is run after a return
 
+    failures += assert::equal(logics.run("less", {2,4}), 1, "2 < 4 = 1");
+    failures += assert::equal(logics.run("less", {4,2}), 0, "4 < 2 = 0");
+
+    failures += assert::equal(logics.run("great", {2,4}), 0, "2 > 4 = 0");
+    failures += assert::equal(logics.run("great", {4,2}), 1, "4 > 2 = 1");
+
+    failures += assert::equal(logics.run("lequal", {2,4}), 1, "2 <= 4 = 1");
+    failures += assert::equal(logics.run("lequal", {2,2}), 1, "2 <= 2 = 1");
+    failures += assert::equal(logics.run("lequal", {4,2}), 0, "4 <= 2 = 0");
+
+    failures += assert::equal(logics.run("gequal", {2,4}), 0, "2 >= 4 = 0");
+    failures += assert::equal(logics.run("gequal", {2,2}), 1, "2 >= 2 = 1");
+    failures += assert::equal(logics.run("gequal", {4,2}), 1, "4 >= 2 = 1");
+
+    failures += assert::equal(logics.run("equal", {2,4}), 0, "2 == 4 = 0");
+    failures += assert::equal(logics.run("equal", {2,2}), 1, "2 == 2 = 1");
+    failures += assert::equal(logics.run("equal", {4,2}), 0, "4 == 2 = 0");
 
   } catch(const char* c) {
     std::cout << "FAIL (" << c << ")" << std::endl;
